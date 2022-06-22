eab1a6c801b7c86cb02f88b25cf3fa51140dfec5
Batuhan Topcu <topcubatuhan00@gmail.com>
2022-06-22 19:01:57 +0300

    homework 2

---
+++ lessonFourHomeworks/homw-2/src/Concrete/StarbucksCustomerManager.java
@@ -0,0 +1,26 @@
+package Concrete;
+
+import Abstract.BaseCustomerManager;
+import Abstract.ICustomerCheckService;
+import Entities.Customer;
+
+public class StarbucksCustomerManager extends BaseCustomerManager{
+	
+	private ICustomerCheckService customerCheckService;
+	
+	public StarbucksCustomerManager(ICustomerCheckService customerCheckService) {
+		this.customerCheckService = customerCheckService;
+	}
+	
+	@Override
+	public void save(Customer customer) throws Exception{
+		if (customerCheckService.CheckIfRealPerson(customer)) {
+			super.save(customer);
+		}else {
+			throw new Exception("Not a valid person");
+		}
+	}
+
+
+	
+}
---
+++ lessonFourHomeworks/homw-2/src/Main.java
@@ -0,0 +1,15 @@
+import Abstract.BaseCustomerManager;
+import Adapters.MernisServiceAdapter;
+import Concrete.CustomerCheckManager;
+import Concrete.NeroCustomerManager;
+import Concrete.StarbucksCustomerManager;
+import Entities.Customer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		
+		BaseCustomerManager customerManager = new StarbucksCustomerManager(new CustomerCheckManager());
+		customerManager.save(new Customer(1, "Batuhan","Topçu", 2002, 1938));
+		
+	}
+}
